929c24fe1be2009a95ae0f7e37c35a0d06980fd1
Loren Hyseni <hysenlor@students.zhaw.ch>
2024-05-13 23:06:42 +0200

    Clean Test Class 2

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/RomanNumTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/RomanNumTest.java
@@ -6,22 +6,18 @@ import org.junit.jupiter.api.Test;
 
 public class RomanNumTest {
 
+    private RomanNum converter = new RomanNum(); // Eine einzige Instanz für alle Tests
+
     @Test
-    public void testConvertOne() {
-        RomanNum converter = new RomanNum();
-        assertEquals("I", converter.convert(1));
+    public void testConvertOneToThree() {
+        // Array von erwarteten Werten für die Zahlen 1, 2 und 3
+        String[] expected = {"I", "II", "III"};
+        
+        // Schleife, die die Zahlen 1 bis 3 testet
+        for (int number = 1; number <= 3; number++) {
+            assertEquals(expected[number - 1], converter.convert(number),
+                         "Die Konvertierung von " + number + " sollte '" + expected[number - 1] + "' ergeben.");
+        }
     }
-    
-    @Test
-    public void testConvertTwo() {
-        RomanNum converter = new RomanNum();
-        assertEquals("II", converter.convert(2));
-    }
-    
-    @Test
-    public void testConvertThree() {
-        RomanNum converter = new RomanNum();
-        assertEquals("III", converter.convert(3));
-    }
-    
+
 }
