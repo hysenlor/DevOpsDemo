43237a6b6a5a798c765da39164b33fede3d49baf
Loren Hyseni <hysenlor@students.zhaw.ch>
2024-05-13 22:12:49 +0200

    RomanNum Java-Class

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNum.java
@@ -0,0 +1,10 @@
+package ch.zhaw.iwi.devops.demo;
+
+public class RomanNum {
+    public String convert(int number) {
+        if (number == 0) {
+            return "N";
+        }
+        return "";
+    }
+}
