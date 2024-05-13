c0fe4b309ba8b8c9189d188281cef0b3dcec6942
Loren Hyseni <hysenlor@students.zhaw.ch>
2024-05-13 22:49:00 +0200

    RomanNum 2

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNum.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNum.java
@@ -2,9 +2,14 @@ package ch.zhaw.iwi.devops.demo;
 
 public class RomanNum {
     public String convert(int number) {
-        if (number == 0) {
-            return "N";
+        if (number == 1) {
+            return "I";
+        } else if (number == 2) {
+            return "II";
+        } else if (number == 3) {
+            return "III";
         }
         return "";
     }
+
 }
