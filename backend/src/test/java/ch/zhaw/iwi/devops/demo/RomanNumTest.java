8442f211731ba8e9241d399d061100d98974ac1a
Loren Hyseni <hysenlor@students.zhaw.ch>
2024-05-13 23:50:07 +0200

    Test Java Class 3

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/RomanNumTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/RomanNumTest.java
@@ -9,15 +9,13 @@ public class RomanNumTest {
     private RomanNum converter = new RomanNum(); // Eine einzige Instanz für alle Tests
 
     @Test
-    public void testConvertOneToThree() {
+    public void testConvertOneToFour() {
         // Array von erwarteten Werten für die Zahlen 1, 2 und 3
-        String[] expected = {"I", "II", "III"};
+        String[] expected = {"I", "II", "III","IV"};
         
         // Schleife, die die Zahlen 1 bis 3 testet
-        for (int number = 1; number <= 3; number++) {
-            assertEquals(expected[number - 1], converter.convert(number),
-                         "Die Konvertierung von " + number + " sollte '" + expected[number - 1] + "' ergeben.");
+        for (int number = 1; number <= 4; number++) {
+            assertEquals(expected[number - 1], converter.convert(number));
         }
     }
-
 }
