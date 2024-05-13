df628169095c1cf3f4161eec4a2dd2bf16af9d19
Loren Hyseni <hysenlor@students.zhaw.ch>
2024-05-13 23:03:24 +0200

    Clean Java Class 2

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNum.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNum.java
@@ -2,14 +2,12 @@ package ch.zhaw.iwi.devops.demo;
 
 public class RomanNum {
     public String convert(int number) {
-        if (number == 1) {
-            return "I";
-        } else if (number == 2) {
-            return "II";
-        } else if (number == 3) {
-            return "III";
+        StringBuilder result = new StringBuilder();
+        while (number >= 1 && number <= 3) {
+            result.append("I");
+            number -= 1;
         }
-        return "";
+        return result.toString();
     }
 
 }
