0cbe9e81b018cb7c26b475ee986974c9489ed4aa
Loren Hyseni <hysenlor@students.zhaw.ch>
2024-05-18 22:22:13 +0200

    Java Class 6

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNum.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNum.java
@@ -4,16 +4,21 @@ public class RomanNum {
     public String convert(int number) {
         StringBuilder result = new StringBuilder();
         
-        if (number == 10){
+        if (number >= 10){
             result.append("X");
-        } else if (number == 5){
+            number -=10;
+        }
+        
+        if (number == 5){
             result.append("V");
-        } else if (number == 4){
+        }
+        
+        if (number == 4){
             result.append("IV");
-        } else {
-            for (int i = 1; i <= number; i++) {
+        } 
+        
+        for (int i = 1; i <= number; i++) {
                 result.append("I");
-            }
         }
 
         return result.toString();
