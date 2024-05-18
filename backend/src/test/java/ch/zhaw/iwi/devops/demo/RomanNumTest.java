4acc25b9c0a92735cb29fcbe7c01523123d21f73
Loren Hyseni <hysenlor@students.zhaw.ch>
2024-05-18 18:05:21 +0200

    Test-Class mit Annotationen 4

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/RomanNumTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/RomanNumTest.java
@@ -10,10 +10,8 @@ public class RomanNumTest {
 
     @Test
     public void testConvertOneToFour() {
-        // Array von erwarteten Werten für die Zahlen 1, 2 und 3
         String[] expected = {"I", "II", "III","IV"};
         
-        // Schleife, die die Zahlen 1 bis 3 testet
         for (int number = 1; number <= 4; number++) {
             assertEquals(expected[number - 1], converter.convert(number));
         }
